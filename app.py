30a93327297d6bb18b51cb0edfc65e442ebae4d4
Dr Dilek Celik <dilekcell40@gmail.com>
2025-06-28 14:44:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import sklearn
 from sklearn.model_selection import train_test_split
 import xgboost as xgb
 from sklearn.metrics import classification_report, accuracy_score, precision_score, f1_score, recall_score
