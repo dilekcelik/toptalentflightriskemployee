36c258f7474be1a0bda9b17d46591f4edd3af239
Dr Dilek Celik <dilekcell40@gmail.com>
2025-06-28 15:19:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 from sklearn.model_selection import train_test_split
-from xgboost import xgboost.XGBClassifier
+from xgboost import XGBClassifier
 from sklearn.metrics import classification_report, accuracy_score, precision_score, f1_score, recall_score
 
 # App title
